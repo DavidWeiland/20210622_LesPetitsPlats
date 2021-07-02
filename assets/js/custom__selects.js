41dd8daa3cfe9e2ba357b17df149b874d76d6ed0
David Weiland <weiland.david@icloud.com>
2021-07-02 13:44:12 +0200

    tags

--- assets/js/custom__selects.js
+++ assets/js/custom__selects.js
@@ -1,37 +1,50 @@
+const tags = document.querySelector(".tags");
 
+let triValue = ["", "", ""];
 export let customisation = function () {
   const customSelect = document.getElementsByClassName("custom-select");
-  for (var i = 0; i < customSelect.length; i++) {
-    var selectCopy = customSelect[i].getElementsByTagName("select")[0];
-    var selectSelected = document.createElement("div");
-    var selectItems = document.createElement("div");
+  for (let i = 0; i < customSelect.length; i++) {
+    const selectCopy = customSelect[i].getElementsByTagName("select")[0];
+    const selectSelected = document.createElement("div");
+    const selectItems = document.createElement("div");
     if (customSelect[i].getAttribute("class") === "custom-select ingredient") {
       selectSelected.setAttribute("class", "select-selected ingredient");
+      selectSelected.setAttribute("value", "ingredient");
       selectItems.setAttribute("class", "select-items select-hide ingredient select-items-ingredient");
     } else if (customSelect[i].getAttribute("class") === "custom-select appliance") {
       selectSelected.setAttribute("class", "select-selected appliance");
+      selectSelected.setAttribute("value", "appliance");
       selectItems.setAttribute("class", "select-items select-hide appliance");
     } else if (customSelect[i].getAttribute("class") === "custom-select ustensil") {
       selectSelected.setAttribute("class", "select-selected ustensil");
+      selectSelected.setAttribute("value", "ustensil");
       selectItems.setAttribute("class", "select-items select-hide ustensil");
     }
     selectSelected.innerHTML = selectCopy.options[selectCopy.selectedIndex].innerHTML;
     customSelect[i].appendChild(selectSelected);
 
-    for (var j = 1; j < selectCopy.length; j++) {
-      var optionElement = document.createElement("div");
+    for (let j = 1; j < selectCopy.length; j++) {
+      const optionElement = document.createElement("div");
       optionElement.innerHTML = selectCopy.options[j].innerHTML;
-      optionElement.addEventListener("click", trier);//action sur click sur l'élément
+      optionElement.addEventListener("click", trier);
       function trier() {
-        var selectOrigine = this.parentNode.parentNode.getElementsByTagName("select")[0];
-        var selectOrigineCiblePrev = this.parentNode.previousSibling;
-        for (var k = 0; k < selectOrigine.length; k++) {
+        const selectOrigine = this.parentNode.parentNode.getElementsByTagName("select")[0];
+        const selectOrigineCiblePrev = this.parentNode.previousSibling;
+        for (let k = 0; k < selectOrigine.length; k++) {
           if (selectOrigine.options[k].innerHTML == this.innerHTML) {
             selectOrigine.selectedIndex = k;
             selectOrigineCiblePrev.innerHTML = this.innerHTML;
-            var sameSelected = this.parentNode.getElementsByClassName("same-as-selected");
-            triValue = document.querySelector("#triSelect").value;
-            for (l = 0; l < sameSelected.length; l++) {
+            const selectValue = selectOrigineCiblePrev.getAttribute("value");
+            if (selectValue === "ingredient") {
+              triValue.splice(0, 1, this.innerHTML);
+            } if (selectValue === "appliance") {
+              triValue.splice(1, 1, this.innerHTML);
+            } if (selectValue === "ustensil") {
+              triValue.splice(2, 1, this.innerHTML);
+            }
+            tagsShower(selectValue);
+            const sameSelected = this.parentNode.getElementsByClassName("same-as-selected");
+            for (var l = 0; l < sameSelected.length; l++) {
               sameSelected[l].removeAttribute("class");
             }
             this.setAttribute("class", "same-as-selected");
@@ -55,9 +68,9 @@ export let customisation = function () {
 }
 
 function closeAllSelect(elmnt) {
-  var i, arrNo = [];
-  var selectItems = document.getElementsByClassName("select-items");
-  var selectSelected = document.getElementsByClassName("select-selected");
+  let i, arrNo = [];
+  const selectItems = document.getElementsByClassName("select-items");
+  const selectSelected = document.getElementsByClassName("select-selected");
   for (i = 0; i < selectSelected.length; i++) {
     if (elmnt == selectSelected[i]) {
       arrNo.push(i)
@@ -74,8 +87,10 @@ function closeAllSelect(elmnt) {
 
 
 
-function optionTri(e){
+function optionTri(e) {
+  console.log(triValue)
   e.stopPropagation();
+/*
   if (triValue === "popularité"){ 
     myJsonParse["media"].sort(function(a,b){
       return a.likes-b.likes;
@@ -97,5 +112,25 @@ function optionTri(e){
       return 0;
     });
   }
-    plancheImage(); //renvoi construction
-};
+  plancheImage(); //renvoi construction
+  */
+
+}
+
+function tagsShower(value){
+  console.log(value)
+  const tag = document.createElement('span');
+  tag.setAttribute("class", "tag " + value);
+  let tagText;
+  if (value === "ingredient") {
+    tagText = triValue[0]
+  }
+  if (value === "appliance") {
+    tagText = triValue[1]
+  }
+  if (value === "ustensil") {
+    tagText = triValue[2]
+  }
+  tag.innerHTML = '<span>' + tagText + '</span><i class ="far fa-times-circle"></i>';
+  tags.appendChild(tag)
+}
